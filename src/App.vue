37b254b352fbac9a0855681fd14f1db32270de59
Your Name <you@example.com>
2021-04-04 20:45:33 -0500

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Would you rather</h1>
+
+    <would-you-rather v-bind:question="questions"
+
+
+    v-bind:answer1="questions[0].answerOne"
+    v-bind:answer2="questions[0].answerTwo"
+
+    v-bind:answer3="questions[1].answerOne"
+    v-bind:answer4="questions[1].answerTwo" 
+
+    v-bind:answer5="questions[2].answerOne"
+    v-bind:answer6="questions[2].answerTwo"  
+
+    v-on:answer-changed="answerChanged"></would-you-rather>
+
+
+    <p>{{ userSelectionMessage}}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data() {
+    return {
+      questions: [
+        {
+          id: 0,
+          question: 'Would you rather dance or sing?',
+          answerOne: 'dance',
+          answerTwo: 'sing'
+        },
+        {
+          id: 1,
+          question: 'Would you rather see a firework display or a circus performance?',
+          answerOne: 'firework display',
+          answerTwo: 'circus performance'
+        },
+        {
+          id: 2,
+          question: 'Would you rather be able to create a new holiday or create a new sport?',
+          answerOne: 'create new holiday',
+          answerTwo: 'create new sport'
+        }
+      ],
+      userSelectionMessage: ''
+    }
+  },
+  methods: {
+    answerChanged(choice) {
+      this.userSelectionMessage = `You Would Rayher... ${choice}`
+    }
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=New+Tegomin&display=swap');
+
+body {
+  background:white
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: new tegomin, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #14212e;
   margin-top: 60px;
+  background: rgb(180, 202, 79)
 }
 </style>
+
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="wyr">
+    <h2>Please make your choice!</h2>
+
+    
+    <div>
+    <h4>{{ question[0].question }}</h4>
+    <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="choiceMade"
+    ><label>{{ answer1 }}</label>
+
+
+    <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choiceMade"
+    ><label>{{ answer2 }}</label>
+    </div>
+
+
+    <div>
+    <h4>{{ question[1].question }}</h4>
+    <input type="radio" v-model="choice" v-bind:value="answer3" v-on:change="choiceMade"
+    ><label>{{ answer3 }}</label>
+
+
+    <input type="radio" v-model="choice" v-bind:value="answer4" v-on:change="choiceMade"
+    ><label>{{ answer4 }}</label>
+    </div>
+
+    <div>
+    <h4>{{ question[2].question }}</h4>
+    <input type="radio" v-model="choice" v-bind:value="answer5" v-on:change="choiceMade"
+    ><label>{{ answer5 }}</label>
+
+
+    <input type="radio" v-model="choice" v-bind:value="answer6" v-on:change="choiceMade"
+    ><label>{{ answer6 }}</label>
+    </div>
+    
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: {
+    question: String,
+    answer1: String,
+    answer2: String,
+    answer3: String,
+    answer4: String,
+    answer5: String,
+    answer6: String,
+  },
+  data() {
+    return {
+      choice: []
+    }
+  },
+  methods: {
+    choiceMade() {
+      this.$emit('answer-changed', this.choice)
+    }
+  }
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+.wyr {
+  border: 2px black solid;
+  background: rgb(116, 157, 223);
+  margin: 10px
+}
+</style>
